d04337254e036704f798b1c73596f4b5b87f48c8
mayad19-meet <mayad19@meet.mit.edu>
2017-08-09 14:30:09 +0300

    Stopping here

--- waseem.py
+++ waseem.py
@@ -1,30 +1,57 @@
 import turtle
 import random
+score=0
 SIZE_X= 1000
 SIZE_Y= 700
 turtle.setup(SIZE_X,SIZE_Y)
 turtle.hideturtle()
+turtle.register_shape("pizza.gif")
+turtle.register_shape("cake.gif")
+turtle.register_shape("cherry.gif")
+turtle.register_shape("chips.gif")
+turtle.register_shape("dounat.gif")
+turtle.register_shape("hamburger.gif")
+
+turtle.shape("triangle")
 food=turtle.clone()
 food.penup()
-turtle.shape('triangle')
 stamp_list=[]
 
 food_pos=[]
 
 food.penup()
+food.shape("pizza.gif")
 food.goto(-300,-295)
 food.showturtle()
-food.stamp()
+pizza=food.stamp()
+stamp_list.append(pizza)
+food.shape("cake.gif")
 food.goto(-200,-195)
-food.stamp()
+cake=food.stamp()
+stamp_list.append(cake)
+food.shape("cherry.gif")
 food.goto(-100,-95)
-food.stamp()
+cherry=food.stamp()
+stamp_list.append(cherry)
+food.shape("chips.gif")
 food.goto(0,5)
-food.stamp()
+chips=food.stamp()
+stamp_list.append(chips)
+food.shape("dounat.gif")
 food.goto(100,95)
-food.stamp()
+dounat=food.stamp()
+stamp_list.append(dounat)
+food.shape("hamburger.gif")
 food.goto(200,195)
-food.stamp()
+hamburger=food.stamp()
+stamp_list.append(hamburger)
+
+if player_pos in stamp_list:
+    food.clearstamp()
+    score= score+1
+    turtle.goto(-450,300)
+    turtle.write("your score is: "+score)
+    
 
 
 
